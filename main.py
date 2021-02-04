1a2ee8a5906630f140c0b2ef2fe98a36955a5e54
tk555 <kondo.t.555@gmail.com>
2021-02-04 18:22:49 +0900

    a

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.options import Options
 
 
 if __name__=='__main__':
-    Options=Options()
+    options=Options()
     options.add_argument('--headless')
     driver=webdriver.Chrome('/usr/bin/chromedriver',options=options)
     driver.get('https://ww.google.co.jp/')
