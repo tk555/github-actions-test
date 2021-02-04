0471d12bb1abd08b562be72ecd68be714c5d4e88
tk555 <kondo.t.555@gmail.com>
2021-02-04 18:20:30 +0900

    add selenium code

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
 if __name__=='__main__':
-    print(3+4+5)
+    Options=Options()
+    options.add_argument('--headless')
+    driver=webdriver.Chrome('/usr/bin/chromedriver',options=options)
+    driver.get('https://ww.google.co.jp/')
+    print(driver.page_source)
